a781f80a51d757adc58e4abcf2e043c0bc6721cc
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-22 02:06:22 +0530

    Add project files.

---
+++ Dynamic binding Run time P2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Dynamic_binding_Run_time_P2
+{
+    class mix_1960
+    {
+        public static void call(mix_1960 m)
+        {
+            m.show();
+        }
+        public virtual void show()
+        {
+            Console.WriteLine("I am Parent :");
+        }
+    }
+    class child_1980:mix_1960
+    {
+        public override void show()
+        {
+            Console.WriteLine("I am child-1");
+        }
+    }
+    class child_1982:child_1980
+    {
+        public override void show()
+        {
+            Console.WriteLine("I am child-2");
+        }
+    }
+    class Demo_2020:child_1982
+    {
+        static void Main(string[] args)
+        {
+            mix_1960 p = new mix_1960();
+            child_1980 ch_1 = new child_1980();
+            child_1982 ch_2 = new child_1982();
+            mix_1960.call(p);
+            mix_1960.call(ch_1);
+            mix_1960.call(ch_2);
+
+        }
+    }
+}
